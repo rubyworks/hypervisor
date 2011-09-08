005a4f6f9b48943b11e52e08d6910beb3237d0b3
7rans <transfire@gmail.com>
2011-09-08 02:57:43 -0400

    Better detail layout, keyboard shortcuts and other improvements.

--- assets/javascripts/hypervisor.js
+++ assets/javascripts/hypervisor.js
@@ -1,4 +1,5 @@
-var documentation;
+var documentation = {};
+var documentation_by_id = {};
 
 //
 String.prototype.escapeHTML = function () {
@@ -10,35 +11,252 @@ String.prototype.escapeHTML = function () {
   );                                          
 };
 
-function toggleBox(boxId, key) {
-  data = documentation[key];
-  type = data['!'];
-  $('#infobox').append($('#template-' + type + '-info').jqote(data));
-  //boxId = boxId.replace(/[.,?!\s]/g, "_"); 
-  $('.box').hide();
-  $(boxId).toggle();
-  $(boxId).find('pre code').each(function(i, e){hljs.highlightBlock(e, '  ')});
-};
-
-// TODO: better way please
-function cleanId(anId) {
-  anId = anId.replace(/\</g, "-lt-");
-  anId = anId.replace(/\>/g, "-gt-");
-  anId = anId.replace(/\=/g, "-eq-");
-  anId = anId.replace(/\?/g, "-qu-");
-  anId = anId.replace(/\!/g, "-ex-");
-  anId = anId.replace(/\~/g, "-tl-");
-  anId = anId.replace(/\[/g, "-bra-");
-  anId = anId.replace(/\]/g, "-ket-");
-  anId = anId.replace(/\#/g, "-hsh-");
-  anId = anId.replace(/\./g, "-dot-");
-  anId = anId.replace(/\:/g, "-cl-");
-  anId = anId.replace(/[/]/g, "-sl-");
-  //anId = anId.replace(/\W+/g, "_");
-  return(anId);
-};
-
 //
+HyperVisor = {
+
+  //
+  bootup: function() {
+    var urlVars = HyperVisor.getUrlVars();
+    var url     = urlVars['doc'] || 'doc.json'
+
+    $.getJSON(url, function(data) {
+      // set up documentation
+
+      // alphabetical table
+      table = new Array();
+      $.each(data, function(key, doc) {
+        doc.key = key;  // ensure key is the same
+        doc.id  = 'api-' + doc['!'] + '-' + HyperVisor.cleanId(key);
+        table.push(doc);
+        // by key
+        documentation[doc.key] = doc;
+        // by id
+        documentation_by_id[doc.id] = doc;
+      });
+      table.sort(HyperVisor.compareNames);
+
+      documentation = data;  // global variable
+      metadata      = data['(metadata)'];
+
+      $('#header-title').append($('#template-header-title').jqote(metadata));
+      $('#header-resources').append($('#template-header-resources').jqote(metadata));
+
+      $.each(table, function(index, value) {
+        type = value['!'];
+        switch(type) {
+        case 'function':
+          $('#methods').append($('#template-method-link').jqote(value));
+          //$('#infobox').append($('#template-function-info').jqote(value));
+          break;
+        case 'method':
+          $('#methods').append($('#template-method-link').jqote(value));
+          //$('#infobox').append($('#template-method-info').jqote(value));
+          break;
+        case 'attribute':
+          $('#methods').append($('#template-method-link').jqote(value));
+          break;
+        case 'class':
+          $('#classes').append($('#template-class-link').jqote(value));
+          //$('#infobox').append($('#template-module-info').jqote(value));
+          break;
+        case 'module':
+          $('#classes').append($('#template-module-link').jqote(value));
+          //$('#infobox').append($('#template-module-info').jqote(value));
+          break;
+        case 'file':
+          $('#file').append($('#template-file-link').jqote(value));
+          //$('#infobox').append($('#template-file-info').jqote(value));
+          break;
+        case 'script':
+          $('#file').append($('#template-file-link').jqote(value));
+          //$('#infobox').append($('#template-script-info').jqote(value));
+          break;
+        };
+      });
+
+      // TODO: how to generalize? also add '/' prefix in future.
+      //toggleBox('#api-README-dot-rdoc', 'README.rdoc');
+      // $('#infobox').append($('#template-file-info').jqote(documentation['README.rdoc']));
+      // $('#api-README-dot-rdoc').show();
+
+      //$(".scroll").scrollable({mousewheel: true}).navigator();
+
+      $('input#search-input').quicksearch('.items li');
+
+      $('input#search-letter').quicksearch('.items li', {
+        'prepareQuery': function (val) {
+          return new RegExp('^'+val, 'i');
+        },
+        'testQuery': function (query, txt, _row) {
+          return query.test(txt);
+        },
+        'bind': 'keyup'
+      });
+
+      // CENTRAL CONTROL
+      $.history.init(function(hash){
+        if(hash == "") {          
+          HyperVisor.toggleBox('api-file-README-rdoc');
+        } else {
+          HyperVisor.toggleBox(hash);   // restore the state from hash
+        }
+      });
+    });
+
+    $(".scroll").mouseover(function(event) {
+      var ul  = $(this).find('ul');
+      var li  = ul.find('li:visible');
+      var sum = 0;
+      li.each( function(){ sum += $(this).outerWidth(); });
+      ul.width(sum);
+    });
+
+    $(".scroll").mousewheel(function(event, delta) {
+      this.scrollLeft -= (delta * event.pageX);
+      event.preventDefault();
+    });
+
+    // Build ABC filter bar.
+    $('#abctabs').append($('#template-abctabs').jqote());
+    var letters = '';
+    for(i=65;i<91;i++){
+      letters = letters + ',' + String.fromCharCode(i);
+    };
+    letters = letters + ',backspace'
+    key(letters, function(event, handler){
+      var s = handler.shortcut;
+      if(s == 'backspace'){s = ' '};
+      $("input#search-letter").val(s).trigger('keyup');
+      $('#clear-button').focus();
+    });
+
+  },
+
+  cleanId: function(anId) {
+    // anId = encodeURIComponent(anId);  DID NOT WORK
+    anId = anId.replace(/\</g,  "-l-");
+    anId = anId.replace(/\>/g,  "-g-");
+    anId = anId.replace(/\=/g,  "-e-");
+    anId = anId.replace(/\?/g,  "-q-");
+    anId = anId.replace(/\!/g,  "-x-");
+    anId = anId.replace(/\~/g,  "-t-");
+    anId = anId.replace(/\[/g,  "-b-");
+    anId = anId.replace(/\]/g,  "-k-");
+    anId = anId.replace(/\#/g,  "-h-");
+    anId = anId.replace(/\./g,  "-d-");
+    anId = anId.replace(/\:\:/g,"-C-");
+    anId = anId.replace(/\:/g,  "-c-");
+    anId = anId.replace(/[/]/g, "-s-");
+    anId = anId.replace(/\W+/g, "-");  // TOO GENERAL?
+    return(anId);
+  },
+
+  toggleBox: function(boxId) {
+    var id = this.cleanId(boxId);
+    //if ($(boxId).length == 0) {
+      data = documentation_by_id[id];
+      $('#infobox').append($('#template-' + data['!'] + '-info').jqote(data));
+      $('.box').hide();
+      $('#'+id).toggle();
+      $('#'+id).find('pre code').each(function(i, e){hljs.highlightBlock(e, '  ')});
+    //} else {
+    //  $('.box').hide();
+    //  $(boxId).toggle();
+    //}
+  },
+
+  toggleBoxByKey: function(key) {
+      boxId = this.cleanId(key);
+    //if ($(boxId).length == 0) {
+      data = documentation[key];
+      $('#infobox').append($('#template-' + data['!'] + '-info').jqote(data));
+      //boxId = boxId.replace(/[.,?!\s]/g, "_"); 
+      $('.box').hide();
+      $(boxId).toggle();
+      $(boxId).find('pre code').each(function(i, e){hljs.highlightBlock(e, '  ')});
+    //} else {
+    //  $('.box').hide();
+    //  $(boxId).toggle();
+    //}
+  },
+
+  //
+  compareNames: function(a, b){
+    if (a.name < b.name) {return -1}
+    if (a.name > b.name) {return 1}
+    return 0;
+  },
+
+  //
+  viewMetaRecursive: function(data) {
+    var html = '<ul>';
+    for (var k in data) {
+      if (data[k] instanceof Object) {
+        html = html + '<li>' + k + ' ' + HyperVisor.viewMetaRecursive(data[k]);
+      } else {
+        html = html + '<li>' + k + ' <code>' + ('' + data[k]).escapeHTML() + '</code>';
+      }
+      html = html + '</li>';
+    }
+    html = html + '</ul>';
+    return html;
+  },
+
+  //
+  viewMeta: function(){
+    $.getJSON('doc.json', function(data) {
+      $('.box').hide();
+      $('#raw').show();
+      $('#raw').append(HyperVisor.viewMetaRecursive(data));
+    });
+  },
+
+  //
+  clickABCTab: function(letter) {
+    $("input#search-letter").val(letter).trigger('keyup');
+  },
+
+  //
+  getUrlVars: function() {
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+      hash = hashes[i].split('=');
+      vars.push(hash[0]);
+      vars[hash[0]] = hash[1];
+    }
+    return vars;
+  },
+
+  resetFontSize: function() {
+    var ourText = $('html,table');
+    ourText.css({fontSize: '1em'});
+  },
+
+  increaseFontSize: function() {
+    var ourText = $('html,table');
+    var currFontSize = ourText.css('fontSize');
+    var finalNum = parseFloat(currFontSize, 10);
+    var stringEnding = currFontSize.slice(-2);
+    finalNum *= 1.2;
+    ourText.css({fontSize: finalNum + stringEnding});
+  },
+
+  decreaseFontSize: function() {
+    var ourText = $('html,table');
+    var currFontSize = ourText.css('fontSize');
+    var finalNum = parseFloat(currFontSize, 10);
+    var stringEnding = currFontSize.slice(-2);
+    finalNum /= 1.2;
+    ourText.css({fontSize: finalNum + stringEnding});
+  }
+
+};
+
+
+
+/*
 function sortObj(arr){
   // Setup Arrays
   var sortedKeys = new Array();
@@ -54,52 +272,5 @@ function sortObj(arr){
   }
   return sortedObj;
 };
-
-//
-function compareNames(a, b){
-  if (a.name < b.name) {return -1}
-  if (a.name > b.name) {return 1}
-  return 0;
-};
-
-//
-function showRaw(){
-  $.getJSON('doc.json', function(data) {
-    $('.box').hide();
-    $('#raw').show();
-    $('#raw').append(recursive_parse(data));
-  });
-};
-
-//
-function recursive_parse(data) {
-  var html = '<ul>';
-  for (var k in data) {
-    if (data[k] instanceof Object) {
-      html = html + '<li>' + k + ' ' + recursive_parse(data[k]);
-    } else {
-      html = html + '<li>' + k + ' <code>' + ('' + data[k]).escapeHTML() + '</code>';
-    }
-    html = html + '</li>';
-  }
-  html = html + '</ul>';
-  return html;
-};
-
-function clickABCTab(letter) {
-  $("input#search-input").val(letter).trigger('keyup');
-};
-
-function getUrlVars()
-{
-  var vars = [], hash;
-  var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-  for(var i = 0; i < hashes.length; i++)
-  {
-    hash = hashes[i].split('=');
-    vars.push(hash[0]);
-    vars[hash[0]] = hash[1];
-  }
-  return vars;
-}
+*/
 
